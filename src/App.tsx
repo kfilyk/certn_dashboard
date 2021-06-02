0678425b69c2278b09a5514f4993251308a2b0f1
Cblands <48147185+Cblands@users.noreply.github.com>
2021-06-01 17:52:21 -0700

    Minor clean up

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// Browser routing typically happens in this file.
+
+export function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -14,5 +16,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
