261822f09be32223b0323ae45a922817d143c159
suuuudo <sudonyym@gmail.com>
2021-06-07 14:04:15 -0700

    Added basic useEffect/useState testing for typescript react

--- src/components/SearchApp/Softcheck.tsx
+++ src/components/SearchApp/Softcheck.tsx
@@ -3,7 +3,31 @@ import React, { useState, useEffect } from 'react';
 import { Softcheck } from '../../api/Certn-Api-Mock/index';
 
 const SoftCheck = () => {
+    const [displayButton, setDisplayButton] = useState({
+        display: false
+    })
 
+    const [showH1, setShowH1] = useState({
+        display: false
+    })
+
+    useEffect( () => { 
+        if (displayButton.display == false ){
+            setShowH1({display: false})
+        } else {
+            setShowH1({display: true})
+        }
+
+    }, [displayButton.display]) // triggers when changed. if [], triggers on initialization
+
+
+    const buttonHandler = (event: React.FormEvent<HTMLButtonElement>) => {
+        if (displayButton.display == false) {
+            setDisplayButton({display: true})
+        } else {
+            setDisplayButton({display: false})
+        }
+    }
 
     const submit = (event: React.FormEvent<HTMLButtonElement>) => {
         event.preventDefault();
@@ -12,10 +36,14 @@ const SoftCheck = () => {
     }
 
     return (
+
         <div>
-            <form>
-                <button type="submit" onClick={submit}>Generate SoftCheck</button>
-            </form>
+        <button onClick={buttonHandler}>Show SoftCheck</button>
+
+        <form>
+            {displayButton.display && <button type="submit" onClick={submit}>Generate SoftCheck</button>}
+        </form>
+        {displayButton.display && <h1>Hello</h1>}
         </div>
     );
 }
