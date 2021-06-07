1ddd5a6879f8f46474978372781935355e204cb9
Reed McIlwain <reedmcilwain@gmail.com>
2021-06-07 15:38:24 -0700

    made linting changes

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,9 +1,9 @@
-<<<<<<< HEAD
 import React, { useState } from 'react';
 // import { setConstantValue } from 'typescript';
 import { Login } from '../../api/Certn-Api/index';
 
-const LoginForm = () => {
+
+const LoginForm = (): JSX.Element => {
 
   const [formInputs, setFormInputs] = useState({
     email: '',
@@ -32,19 +32,12 @@ const LoginForm = () => {
   return (
     <div>
       <form>
-        <input type="text" name="username" placeholder="Email" onChange={emailHandler} value={formInputs.email}/>
-        <input type="password" name="password" placeholder="Password" onChange={passwordHandler}value={formInputs.password}/>
-        <input type="submit" onClick={submit}/>
+        <input type='text' name='username' placeholder='Email' onChange={emailHandler} value={formInputs.email}/>
+        <input type='password' name='password' placeholder='Password' onChange={passwordHandler}value={formInputs.password}/>
+        <input type='submit' onClick={submit}/>
       </form>
     </div>
   );
 }
 
 export default LoginForm;
-=======
-
-export function Login(): JSX.Element {
-  return <div>Login</div>;
-}
-
->>>>>>> linting fixes needed
