93707c241b5c91f46ad3d80d26b962688194e592
Cblands <48147185+Cblands@users.noreply.github.com>
2021-06-16 13:37:02 -0700

    Update Search.tsx

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,7 +1,21 @@
+import { UserProvider } from '../../userContext';
+import { certnTheme } from '../../Theme/certn-theme';
+import styled, { ThemeProvider } from 'styled-components';
+
+const AppDiv = styled.div`
+    font-family: ${(props) => props.theme.fontFamily};
+`;
+
 const Search = (): JSX.Element => (
-    <div>
-        <p>Welcome to the Search Page!</p>
-    </div>
+    <ThemeProvider theme={certnTheme}>
+        <UserProvider>
+            <AppDiv>
+                <div>
+                    <p>Welcome to the Search Page!</p>
+                </div>
+            </AppDiv>
+        </UserProvider>
+    </ThemeProvider>
 );
 
 export default Search;
