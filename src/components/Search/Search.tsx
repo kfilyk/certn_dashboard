28563a06bf4980597a14337608e768ac44e4963e
Cblands <48147185+Cblands@users.noreply.github.com>
2021-06-16 13:20:43 -0700

    remove redundant wrappers

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,32 +1,7 @@
-import { UserProvider, WithUser } from '../../userContext';
-import { certnTheme } from '../../Theme/certn-theme';
-import styled, { ThemeProvider } from 'styled-components';
-import { Redirect } from 'react-router-dom';
-import { LogoutButton } from '../Login/LoginSC';
-
-const AppDiv = styled.div`
-    font-family: ${(props) => props.theme.fontFamily};
-`;
-
-const Search = (): JSX.Element => {
-    const { token, userLogout } = WithUser();
-
-    return (
-        <ThemeProvider theme={certnTheme}>
-            <UserProvider>
-                <AppDiv>
-                    {token ? (
-                        <LogoutButton onClick={() => userLogout()}>Log Out</LogoutButton>
-                    ) : (
-                        <Redirect to="/login" />
-                    )}
-                    <div>
-                        <p>Welcome to the Search Page!</p>
-                    </div>
-                </AppDiv>
-            </UserProvider>
-        </ThemeProvider>
-    );
-};
+const Search = (): JSX.Element => (
+    <div>
+        <p>Welcome to the Search Page!</p>
+    </div>
+);
 
 export default Search;
