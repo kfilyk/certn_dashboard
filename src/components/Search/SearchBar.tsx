1c448ca6ad57d8be0c361c84b4f004f18b3df123
Alex Schell <aschell@uvic.ca>
2021-06-18 18:57:30 -0700

    Add in advanced search items

--- src/components/Search/SearchBar.tsx
+++ src/components/Search/SearchBar.tsx
@@ -1,17 +1,42 @@
 // Ant Design
 import { SearchOutlined } from '@ant-design/icons';
-import { SearchForm, SearchWrapper, SearchButton } from './SearchSC';
+import { SearchForm, SearchWrapper, SearchButton, AdvancedSearchItem, AdvancedWapper } from './SearchSC';
+import { Form, Input } from 'antd';
+
+const BasicSearch = (): JSX.Element => (
+    <SearchForm
+        prefix={<SearchOutlined />}
+        placeholder="Input search text"
+        enterButton="Advanced"
+        style={{ width: '500' }}
+    />
+);
+
+const AdvancedSearch = (): JSX.Element => (
+    <AdvancedWapper>
+        <AdvancedSearchItem label="First Name">
+            <Input placeholder="Candidate First Name" />
+        </AdvancedSearchItem>
+        <AdvancedSearchItem label="Last Name">
+            <Input placeholder="Candidate First Name" />
+        </AdvancedSearchItem>
+        <AdvancedSearchItem label="Phone Number">
+            <Input placeholder="Candidate Phone Number" />
+        </AdvancedSearchItem>
+        <AdvancedSearchItem label="Email">
+            <Input placeholder="Candidate Email" />
+        </AdvancedSearchItem>
+    </AdvancedWapper>
+);
+
+const SearchCommit = (): JSX.Element => <SearchButton type="primary">Search</SearchButton>;
 
 const SearchBar = (): JSX.Element => (
     <SearchWrapper>
-        <SearchForm
-            prefix={<SearchOutlined />}
-            placeholder="Input search text"
-            enterButton="Advanced"
-            style={{ width: '500' }}
-        />
-        <SearchButton type="primary"> Search</SearchButton>
+        <BasicSearch />
+        <SearchCommit />
+        <AdvancedSearch />
     </SearchWrapper>
 );
 
-export default SearchBar;
+export { SearchBar };
--- src/components/Search/SearchSC.tsx
+++ src/components/Search/SearchSC.tsx
@@ -1,15 +1,18 @@
 import styled from 'styled-components';
-import { Input, Button } from 'antd';
+import { Input, Button, Form } from 'antd';
 const { Search } = Input;
 
 export const SearchWrapper = styled.div`
-    width: 500px;
+    margin-top: 16px;
+    margin-left: 16px;
+    width: 100%;
     display: flex;
-    margin-left 16px;
+    align-items: center;
 `;
 
 export const SearchForm = styled(Search)`
     color: ${(props) => props.theme.color.green[400]};
+    width: 25%;
     .ant-btn-primary {
         background-color: ${(props) => props.theme.color.gray[200]};
         border-color: ${(props) => props.theme.color.gray[200]};
@@ -19,6 +22,7 @@ export const SearchForm = styled(Search)`
 
 export const SearchButton = styled(Button)`
     margin-left: 16px;
+    margin-right: 16px;
     background-color: ${(props) => props.theme.color.green[400]};
     color: ${(props) => props.theme.color.white};
     border-color: ${(props) => props.theme.color.green[400]};
@@ -28,4 +32,15 @@ export const SearchButton = styled(Button)`
         background-color: ${(props) => props.theme.color.green[400]};
         border-color: ${(props) => props.theme.color.green[400]};
     }
+`;
+
+export const AdvancedSearchItem = styled(Form.Item)`
+    margin-left: 16px;
+    width: 20%;
+    display: inline-block;
+`;
+
+export const AdvancedWapper = styled(Form)`
+    margin-left: 0px;
+    margin-bottom: 30px;
 `;
