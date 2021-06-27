427c5364dd4c5edc8880057c920a1155cb9c91d6
Reed McIlwain <reedmcilwain@gmail.com>
2021-06-27 11:41:00 -0700

    Final changes to search bar

--- src/components/Search/SearchBar.tsx
+++ src/components/Search/SearchBar.tsx
@@ -1,5 +1,12 @@
 // Ant Design
-import { SearchOutlined, UserOutlined, PhoneOutlined, MailOutlined } from '@ant-design/icons';
+import {
+    SearchOutlined,
+    UserOutlined,
+    PhoneOutlined,
+    MailOutlined,
+    MenuUnfoldOutlined,
+    MenuFoldOutlined,
+} from '@ant-design/icons';
 import {
     SearchForm,
     SearchWrapper,
@@ -7,9 +14,13 @@ import {
     AdvancedSearchItem,
     AdvancedWapper,
     AdvancedSwitch,
-} from './SearchSC';
+    ToggleButtonWrapper,
+    BasicSearchWrapper,
+    InputWrapper,
+    TextWrapper,
+} from './SearchBarSC';
 import { Input } from 'antd';
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
 const regExp = /[a-zA-Z]/g;
 
@@ -32,50 +43,24 @@ interface SearchProps {
     }) => Promise<void>;
 }
 
-// const BasicSearch = (): JSX.Element => (
-//     <SearchForm name="basic" prefix={<SearchOutlined />} placeholder="Input search text" style={{ width: '500' }} />
-// );
-
-// Declare types of args to be passed
-type toggleProps = {
-    showAd: boolean;
-    setShowAd: any;
-};
-
-// Basic search form with advanced mode toggle
-const BasicSearch = ({ showAd, setShowAd }: toggleProps): JSX.Element => (
-    <SearchForm
-        name="basic"
-        prefix={<SearchOutlined />}
-        placeholder="Input search text"
-        enterButton={<AdvancedToggle showAd={showAd} setShowAd={setShowAd} />}
-        style={{ width: '500' }}
-    />
-);
-
-// Toggles the advanced search inputs
-const AdvancedToggle = ({ showAd, setShowAd }: toggleProps): JSX.Element => (
-    <AdvancedSwitch onClick={() => setShowAd(!showAd)}> Advanced </AdvancedSwitch>
-);
-
 const AdvancedSearch = (): JSX.Element => (
     // Form for advanced Search, with form wrapper and items
     <AdvancedWapper name="Advanced Search">
         <AdvancedSearchItem name="firstname" label="First Name" rules={[{ type: 'string' }]}>
-            <Input prefix={<UserOutlined />} placeholder="Candidate First Name" type="firstname" />
+            <Input prefix={<UserOutlined />} placeholder="Candidate First Name" type="firstname" allowClear />
         </AdvancedSearchItem>
         <AdvancedSearchItem name="lastname" label="Last Name" rules={[{ type: 'string' }]}>
-            <Input prefix={<UserOutlined />} placeholder="Candidate Last Name" type="lastname" />
+            <Input prefix={<UserOutlined />} placeholder="Candidate Last Name" type="lastname" allowClear />
         </AdvancedSearchItem>
         <AdvancedSearchItem name="phone" label="Phone Number" rules={[{ validator: validPhone }]}>
-            <Input prefix={<PhoneOutlined />} placeholder="Candidate Phone #" type="phone" />
+            <Input prefix={<PhoneOutlined />} placeholder="Candidate Phone #" type="phone" allowClear />
         </AdvancedSearchItem>
         <AdvancedSearchItem
             name="email"
             label="Email"
             rules={[{ type: 'email', message: 'Please enter a valid email' }]}
         >
-            <Input prefix={<MailOutlined />} placeholder="Candidate Email" type="email" />
+            <Input prefix={<MailOutlined />} placeholder="Candidate Email" type="email" allowClear />
         </AdvancedSearchItem>
     </AdvancedWapper>
 );
@@ -87,32 +72,48 @@ const SearchCommit = (): JSX.Element => (
     </SearchButton>
 );
 
-// const AdvancedToggle = (initialValue = false): [boolean, () => void] => {
-//     const [toggle, setToggle] = useState(initialValue);
+const AdvancedToggle = (initialValue = false): [boolean, () => void] => {
+    const [toggle, setToggle] = useState(initialValue);
 
-//     const toggleButton = useCallback(() => {
-//         setToggle(!toggle);
-//     }, [toggle]);
+    const toggleButton = useCallback(() => {
+        setToggle(!toggle);
+    }, [toggle]);
 
-//     return [toggle, toggleButton];
-// };
+    return [toggle, toggleButton];
+};
 
 // Actual Searchbar element, contains basic search bar form, submit button and advanced search form
 const SearchBar = (): JSX.Element => {
-    // const [advanced, setAdvanced] = AdvancedToggle();
+    const [advanced, setAdvanced] = AdvancedToggle();
 
-    // return (
-    //     <SearchWrapper>
-    //         <BasicSearch />
-    //         <AdvancedSwitch onClick={setAdvanced}>{advanced ? 'Basic' : 'Advanced'}</AdvancedSwitch>
-    //         {advanced && <AdvancedSearch />}
-    //         <SearchCommit />
-    const [showAd, setshowAd] = useState(false);
     return (
         <SearchWrapper>
-            <BasicSearch showAd={showAd} setShowAd={setshowAd} />
+            <BasicSearchWrapper>
+                <TextWrapper>
+                    <p>Search Applications</p>
+                </TextWrapper>
+                <InputWrapper>
+                    <SearchForm
+                        name="basic"
+                        prefix={<SearchOutlined />}
+                        placeholder="Search All Fields..."
+                        allowClear
+                    />
+                    <AdvancedSwitch onClick={setAdvanced}>
+                        {advanced ? (
+                            <ToggleButtonWrapper>
+                                <MenuFoldOutlined /> <p>Basic</p>
+                            </ToggleButtonWrapper>
+                        ) : (
+                            <ToggleButtonWrapper>
+                                <MenuUnfoldOutlined /> <p>Advanced</p>
+                            </ToggleButtonWrapper>
+                        )}
+                    </AdvancedSwitch>
+                </InputWrapper>
+            </BasicSearchWrapper>
+            {advanced && <AdvancedSearch />}
             <SearchCommit />
-            {showAd && <AdvancedSearch />}
         </SearchWrapper>
     );
 };
