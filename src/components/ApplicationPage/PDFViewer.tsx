146323c26f4433d32aea41c255a9d6e061264d2b
mahumazeem <azeem.mahum@gmail.com>
2021-07-09 10:16:08 -0700

    Added react-pdf package and added a type file for pdf types, pdf display works a little

--- src/components/ApplicationPage/PDFViewer.tsx
+++ src/components/ApplicationPage/PDFViewer.tsx
@@ -1,5 +1,10 @@
 import { useState } from 'react';
 import { PreviewCheckbox, ButtonWrapper, ModalWrapper } from './ApplicationActionsSC';
+import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+
+import logo from '../../logo.svg';
+import test from '../../deleteBeforeRelease/test.pdf';
 
 export const PDFViewer = (): JSX.Element => {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -8,6 +13,9 @@ export const PDFViewer = (): JSX.Element => {
     const [pages, setPages] = useState(null);
 
     const [showModal, setShowModal] = useState(false);
+
+    const [numPages, setNumPages] = useState<number | null>(null);
+    const [pageNumber, setPageNumber] = useState(1);
 
     const displayModal = () => {
         setShowModal(true);
@@ -21,6 +29,23 @@ export const PDFViewer = (): JSX.Element => {
         setShowModal(false);
     };
 
+    const changePage = (offset: number) => {
+        setPageNumber((prevPageNumber) => prevPageNumber + offset);
+    };
+    const previousPage = () => {
+        changePage(-1);
+    };
+
+    const nextPage = () => {
+        changePage(1);
+    };
+
+    // eslint-disable-next-line no-shadow
+    const onDocumentLoadSuccess = ({ numPages }: { numPages: number }) => {
+        setNumPages(numPages);
+        setPageNumber(1);
+    };
+
     //PDF will be implemented inside ModalWrapper above the <p>, which will be used to count the pages
     return (
         <div>
@@ -29,9 +54,18 @@ export const PDFViewer = (): JSX.Element => {
                 Preview
             </ButtonWrapper>
             <ModalWrapper title="Preview" visible={showModal} onOk={handleOk} onCancel={handleCancel}>
+                <Document file={test} onLoadSuccess={onDocumentLoadSuccess}>
+                    <Page pageNumber={pageNumber} />
+                </Document>
                 <p>
-                    Page {page} of {pages}
+                    Page {pageNumber || (numPages ? 1 : '--')} of {numPages || '--'}
                 </p>
+                <ButtonWrapper type="primary" disabled={pageNumber <= 1} onClick={previousPage}>
+                    Previous
+                </ButtonWrapper>
+                <ButtonWrapper type="primary" disabled={pageNumber >= numPages!} onClick={nextPage}>
+                    Next
+                </ButtonWrapper>
             </ModalWrapper>
         </div>
     );
