b584a3c902a75cf9da419cb7143d3c9aac1440f3
KeeganCK <keegankavanagh@live.com>
2021-07-20 19:51:19 -0700

    Rafay helped. Changed background to green

--- src/components/Search/SearchSC.tsx
+++ src/components/Search/SearchSC.tsx
@@ -3,10 +3,10 @@ import { Input, Button, Form } from 'antd';
 
 export const SearchPageWrapper = styled.div`
     background-color: ${(props) => props.theme.color.green[50]};
-    // padding: 50px;
-    // height: 100%;
-    // min-width: 800px;
-    // display: flex;
-    // flex-direction: column;
-    // flex-grow: 2;
+    padding: 50px;
+    height: 100%;
+    min-width: 800px;
+    display: flex;
+    flex-direction: column;
+    flex-grow: 2;
 `;
